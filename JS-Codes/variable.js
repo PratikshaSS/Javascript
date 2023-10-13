f83db093a89cf06bd8d8e2831faecf16c02933eb
sPratikshas <spratiksha208@gmail.com>
2023-10-12 19:03:43 -0500

    12 Oct

---
+++ JS-Codes/variable.js
@@ -0,0 +1,29 @@
+// Variables are the container to store the data(data type)
+// let/var/const : default variables(keywords) followed by variable name
+// Variable Name : rules >>>spaces not allowed, 
+// first letter of variable should always alphabets,
+// special characterss not allowed(except $ and (_)underscore)
+
+let fname;         //declaration of variable //undefined
+fname = 'Poonam';  //definition 
+
+//keyword varname =(assignment operator) ('value/data')
+let myFirstName = 'Pratiksha';   //declaration + definition
+
+
+var mySName = 'Shinde';
+
+var myName;
+myName = 'Om';
+myName = 'Shweta';
+
+
+//const keyword : initialization of value is must
+const cityName = 'Pune';  //can't reassign value
+//cityName = 'Mumbai';
+console.log('cityName', cityName);
+
+
+
+
+
