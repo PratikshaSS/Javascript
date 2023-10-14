c4bcd260204e657d590659adc06964e2ac634cfd
sPratikshas <spratiksha208@gmail.com>
2023-10-14 15:04:55 -0500

    13 Oct

---
+++ JS-Codes/datatypes.js
@@ -0,0 +1,37 @@
+// Javascript has 8 Datatypes >> Primitive
+// 1. String
+// 2. Number
+// 3. Bigint
+// 4. Boolean
+// 5. undefined
+// 6. Null
+// 7. Symbol
+// 8. Object
+
+// The Object Datatype >> Non-primitive
+// 1. An Object
+// 2. An Array
+// 3. A Date
+
+
+let state = '';  //Empty String
+
+let city = 'Pune';  //string datatype
+console.log(city);
+
+city = 'Mumbai';
+console.log(city);
+console.log(city);
+console.log(city);
+
+
+
+
+
+
+
+
+
+
+
+
