fb719f57ac794c576a54aab4b57c966ac2a3317a
sPratikshas <spratiksha208@gmail.com>
2023-10-15 14:51:19 -0500

    14 Oct

--- JS-Codes/datatypes.js
+++ JS-Codes/datatypes.js
@@ -16,7 +16,8 @@
 
 let state = '';  //Empty String
 
-let city = 'Pune';  //string datatype
+//string datatype
+let city = 'Pune';  
 console.log(city);
 
 city = 'Mumbai';
@@ -24,14 +25,35 @@ console.log(city);
 console.log(city);
 console.log(city);
 
+//number Datatype
+var marks = 90;  
+console.log('Marks ',marks);
 
+var mobNo = 7456986423;
+// var mobNo = "7456986423";  //String datatype
+console.log('MobNo ',mobNo);
 
+//boolean datatype 
+var isFavFruit = false;   //true / false
+console.log(isFavFruit);
 
+//typeof() Operator >>  it will return / gives type of variable
 
+console.log('Data type of isFavFruit variable is :',typeof(isFavFruit));
 
+//Undefine Datatype
+const match = undefined;
+console.log(match);
 
+let abc;
+console.log(abc);
+console.log(typeof(abc));
 
+//Null Datatype
+var myName = null;
+console.log(myName);
+console.log(typeof(myName));
 
-
-
-
+//Bigint Datatype
+let a = 9999999999999999;
+console.log(a);
