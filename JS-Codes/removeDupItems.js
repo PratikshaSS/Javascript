2d6af605f339276e7e7897f2421f66333f1aba9f
sPratikshas <spratiksha208@gmail.com>
2023-11-06 17:59:59 -0500

    Remove duplicate items from Array

---
+++ JS-Codes/removeDupItems.js
@@ -0,0 +1,17 @@
+
+// --- Remove Duplicate Items --- 
+
+// 1. new Set() 
+let char = ['a','b','a','d','f','b'];
+let char2 = new Set(char);
+console.log("Remove Duplicate Items using new Set :",char2);    // ['a','b','d','f']; 
+
+
+// 2. filter : returns array
+let data = char.filter((item,index) => {
+    // if(char.indexOf(item) === index){
+    //     return item;
+    // }
+    return char.indexOf(item) === index;
+} )
+console.log("Remove Duplicate Items using filter :",data);
