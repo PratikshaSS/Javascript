f539d0aee1b04c72498d45acf4a37b9f908c03e1
sPratikshas <spratiksha208@gmail.com>
2023-11-09 17:55:35 -0500

    9 nov Ternary Operator

---
+++ JS-Codes/ternaryOp.js
@@ -0,0 +1,12 @@
+// ternary operator(?) >> Same as if else
+
+let marks = 50;
+if(marks > 35){
+    console.log("Pass");
+}
+else{
+    console.log("Fail");
+}
+
+// condition ? statement1(true) : statement2(false)
+marks > 35 ? console.log("Pass") : console.log("Fail");
