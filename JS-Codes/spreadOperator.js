30edd7e9b1923165013b683ba2b7b16adeb88f17
sPratikshas <spratiksha208@gmail.com>
2023-11-09 17:51:51 -0500

    9 Nov Spread Operator

--- JS-Codes/spreadOperator.js
+++ JS-Codes/spreadOperator.js
@@ -20,3 +20,41 @@ let [item1 , item2 , ...item3] = num;
 console.log(item1);     //points to 1st item of an array
 console.log(item2);     //points to 2nd item of an array
 console.log(item3);     //points all remaining items of an array
+
+
+// Concatnation of an array
+let color1 = ['red','orange'];
+let color2 = ['violet','black'];
+let color = [...color1 , ...color2];
+console.log("Concatnation of two arrays :",color);
+
+
+// 3. copy objects
+let emp = {
+    name : 'Sushant',
+    age : '25',
+    city : 'Pune'
+};
+
+let emp3 = {...emp};
+console.log("Copy objects :",emp3);
+
+// concatnation of objects : cant use spread operator for obj
+let emp1 = {
+    name : 'Sumit',
+    age : '25',
+    city : 'Pune',
+    mob : 3243567
+};
+let emp2 = {
+    name : 'Rajesh',
+    age : '25',
+    city : 'Pune',
+    sal : 30000
+};
+let emp4 = {...emp1 , ...emp2}; //use spread operator if keys are diff in both objects
+console.log(emp4);
+
+// const mergeObj = Object.assign({}, target, ...source);
+const mergeObj = Object.assign({},emp1, emp2);
+console.log(mergeObj);
