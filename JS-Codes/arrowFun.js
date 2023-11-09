50e084582b0697480aaacd53addd66b19dc9d14a
sPratikshas <spratiksha208@gmail.com>
2023-11-09 18:02:37 -0500

    9 Nov Arrow Function

---
+++ JS-Codes/arrowFun.js
@@ -0,0 +1,9 @@
+// Arrow Function : allows us to write shorter function syntax.
+function test(){
+    return 20 * 8;
+}
+console.log(test());
+
+// using arrow function
+test1 = ()=> 20*8;
+console.log(test1());
